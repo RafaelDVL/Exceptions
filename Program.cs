6f7b11c087b7c323161ab714882ad820f4259296
RafaelDVL <rafaelrodrigues.dsg3d@gmail.com>
2023-02-01 15:38:04 -0300

    TryCatch

--- Program.cs
+++ Program.cs
@@ -10,13 +10,23 @@ namespace Exceptions
             var arry = new int[6];
 
             try{
-                for(var cont = 0; cont < 10; cont++){
+                for(var cont = 0; cont < 6; cont++){
                     Console.WriteLine(arry[cont]);
                 }
+                Salvar("");
+
             }catch (Exception e){
+                System.Console.WriteLine(e.InnerException);
                 System.Console.WriteLine(e.Message);
+                System.Console.WriteLine("Algo deu errado!");
             }
 
+        }
+
+        static void Salvar(string text){
+            if(string.IsNullOrEmpty(text)){
+                throw new Exception("O texto não pode ser nulo ou vazio!");
+            }
         }
     }
 }
