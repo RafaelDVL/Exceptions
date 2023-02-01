0c115215487ba22023d014f12dbc84b39fa1e65d
RafaelDVL <rafaelrodrigues.dsg3d@gmail.com>
2023-02-01 15:19:09 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exceptions
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            var arry = new int[6];
+
+            try{
+                for(var cont = 0; cont < 10; cont++){
+                    Console.WriteLine(arry[cont]);
+                }
+            }catch (Exception e){
+                System.Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
